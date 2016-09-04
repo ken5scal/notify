190fce18bc434b3df7774630c7da18b25b48d821
ken5scal <kengoscal@gmail.com>
2016-09-04 19:13:44 +0900

    Add real time monitoring and action triggering

--- monitor.go
+++ monitor.go
@@ -4,4 +4,21 @@ type Monitor struct {
 	Paths map[string]string
 }
 
-fun
+func (m *Monitor) Now() (int, error) {
+	var counter int
+	for path, lastHash := range m.Paths {
+		newHash, err := DirHash(path)
+		if err != nil {
+			return 0, err
+		}
+		if newHash != lastHash {
+			err := m.act(path)
+			if err != nil {
+				return counter, err
+			}
+			m.Paths[path] = newHash
+			counter++
+		}
+	}
+	return counter, nil
+}

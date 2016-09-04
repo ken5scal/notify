f988769b327e06b9fa3c65a240bea742ccf06fe3
ken5scal <kengoscal@gmail.com>
2016-09-04 20:08:16 +0900

    import filedb

--- cmds/notifyd/main.go
+++ cmds/notifyd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"flag"
 	"github.com/ken5scal/notify"
+	"github.com/matryer/filedb"
 )
 
 type path struct {
@@ -22,6 +23,7 @@ func main() {
 	var (
 		interval = flag.Int("interval", 10, "Check duration per sec")
 		service = flag.String("service", "slack", "Notify service")
+		dbpath = flag.String("db", "./db", "path to file db")
 	)
 
 	flag.Parse()
@@ -29,5 +31,18 @@ func main() {
 	m := &monitor.Monitor{
 		Paths: make(map[string]string),
 		Service: *service,
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
 	}
 }

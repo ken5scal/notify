f0e8e3baff1774914037ea3f109a13875b0a15ff
ken5scal <kengoscal@gmail.com>
2016-09-04 22:19:58 +0900

    Fixed problem

--- monitor.go
+++ monitor.go
@@ -1,12 +1,7 @@
 package monitor
 
-import (
-	"fmt"
-	"errors"
-)
-
 type Monitor struct {
-	Paths map[string]string
+	Paths   map[string]string
 	Service string
 }
 
@@ -15,13 +10,11 @@ func (m *Monitor) Now() (int, error) {
 	for path, lastHash := range m.Paths {
 		newHash, err := DirHash(path)
 		if err != nil {
-			fmt.Println("hogehoge")
 			return 0, err
 		}
 		if newHash != lastHash {
 			err := m.act(path)
 			if err != nil {
-				fmt.Println("fugafua")
 				return counter, err
 			}
 			m.Paths[path] = newHash
@@ -36,6 +29,5 @@ func (m *Monitor) act(path string) error {
 	//dirname := filepath.Base(path)
 	//filename := fmt.Sprintf("%d.zip", time.Now().UnixNano())
 	//return m.Archiver.Archive(path, filepath.Join(m.Destination, dirname, filename))
-	fmt.Println("Act has bee invoked...")
-	return errors.New("hogehoge")
+	return nil
 }
